cd1833999cad6e42fe73e9c09f5a9844d47c6412
Anshika Yadav <146864436+anshika494@users.noreply.github.com>
2026-04-30 22:09:09 +0530

    Solved Problem-3742

---
+++ Problem-3742.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int maxPathScore(vector<vector<int>>& grid, int k) {
+        int m = grid.size(), n = grid[0].size();
+
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(k + 1, -1)));
+        int startCost = (grid[0][0] == 0 ? 0 : 1);
+
+        if (startCost <= k)
+            dp[0][0][startCost] = grid[0][0];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int c = 0; c <= k; c++) {
+                    if (dp[i][j][c] == -1)
+                        continue;
+                    if (j + 1 < n) {
+                        int val = grid[i][j + 1];
+                        int cost = (val == 0 ? 0 : 1);
+                        int newCost = c + cost;
+
+                        if (newCost <= k) {
+                            dp[i][j + 1][newCost] = max(dp[i][j + 1][newCost], dp[i][j][c] + val);
+                        }
+                    }
+                    if (i + 1 < m) {
+                        int val = grid[i + 1][j];
+                        int cost = (val == 0 ? 0 : 1);
+                        int newCost = c + cost;
+
+                        if (newCost <= k) {
+                            dp[i + 1][j][newCost] = max(dp[i + 1][j][newCost],
+                                                        dp[i][j][c] + val);
+
+                        }
+                    }
+                }
+            }
+        }
+
+        int ans = -1;
+        for (int c = 0; c <= k; c++) {
+            ans = max(ans, dp[m - 1][n - 1][c]);
+        }
+
+        return ans;        
+    }
+};
