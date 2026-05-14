884c67d93006685cfe0f88ef66565dba8d8aaf11
Anshika Yadav <146864436+anshika494@users.noreply.github.com>
2026-05-14 14:45:43 +0530

    Solved Problem-2784

--- Problem-2784.cpp
+++ Problem-2784.cpp
@@ -1,8 +1,13 @@
 class Solution {
 public:
     bool isGood(vector<int>& nums) {
+        int sz = nums.size();
+
+        if (sz < 2)
+            return false;
+
         sort(nums.begin(), nums.end());
-        int n = nums.size() - 1;
+        int n = sz - 1;
 
         for (int i = 0; i < n - 1; i++) {
             if (nums[i] != i + 1)
