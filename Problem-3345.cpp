5dcd3e21cddbd72cace146c45c8746d13761a7f0
Anshika Yadav <146864436+anshika494@users.noreply.github.com>
2026-08-06 16:56:21 +0530

    Solved Problem-3345

---
+++ Problem-3345.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+        while (true) {
+            int x = n;
+            int prod = 1;
+
+            if (x == 0) {
+                prod = 0;
+            } else {
+                while (x > 0) {
+                    prod *= (x % 10);
+                    if (prod == 0) break;
+                    x /= 10;
+                }
+            }
+
+            if (prod % t == 0)
+                return n;
+
+            n++;
+        }
+    }
+};
